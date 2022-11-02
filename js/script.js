48cae71e551aac1c6ee58856413df98915a4e04c
Mari Sumida Miyashiro <mariksm@hawaii.edu>
2022-11-02 02:20:29 -1000

    added arrows to "featured products"

--- js/script.js
+++ js/script.js
@@ -1,12 +1,45 @@
-// OVERLAY NAV MENU SHOW/HIDE //
+document.addEventListener('DOMContentLoaded', function () {
 
-const mymenubutton = document.querySelector('.menu-button');
-const mysitenav = document.querySelector('.site-header .site-nav');
+    // OVERLAY NAV MENU SHOW/HIDE
 
-mymenubutton.onclick = function () {
-    if ( mysitenav.getAttribute('data-navstate') === 'open') {
-    mysitenav.setAttribute('data-navstate', 'closed');
-    } else {
-    mysitenav.setAttribute('data-navstate', 'open');
+    const mymenubutton = document.querySelector('.menu-button');
+    const mysitenav = document.querySelector('.site-header .site-nav');
+
+        mymenubutton.onclick = function () {
+            if ( mysitenav.getAttribute('data-navstate') === 'open') {
+        mysitenav.setAttribute('data-navstate', 'closed');
+        } else {
+        mysitenav.setAttribute('data-navstate', 'open');
+        }
+    };
+
+    // OVERLAY NAV HIDE ON NAV ITEM CLICK
+    var drawernavlinks = document.querySelectorAll(".drawer nav a");
+    var drawerheader = document.querySelector(".drawer .site-nav");
+    var j;
+    for (j = 0; j < drawernavlinks.length; j++) {
+        drawernavlinks[j].onclick = function () {
+            drawerheader.setAttribute('data-navstate', 'closed');
+        };
+    };
+
+    // SCROLL TRIGGERED ANIMATION
+
+    let options = {
+        threshold: .25
     }
-}
+    const myobserver = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+                entry.target.setAttribute("data-sectionstate", "active");
+            } else {
+                entry.target.setAttribute("data-sectionstate", "inactive");
+            }
+        });
+    }, options);
+
+    document.querySelectorAll(".scroll-triggered").forEach((el) => {
+        myobserver.observe(el);
+    });
+
+});
